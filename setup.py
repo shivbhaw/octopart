78fa7cbf41ebf6416e2eccfcf2675b7ed6ebf599
Sean Adler <sean@getpartic.com>
2017-04-14 14:22:33 -0700

    Bump version to 0.0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Octopart",
-    version="0.0.10",
+    version="0.0.11",
     author="Sean Adler",
     author_email="sean@tempoautomation.com",
     license="MIT",
