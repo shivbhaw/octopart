f5b15095303d07e0f2c0b2a88cc353f5d658bee4
Jonas Neubert <jonas@tempoautomation.com>
2017-07-13 14:40:54 -0700

    making it clear via OSI classifiers that we are Python 3.6 only

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development",
     ],
     install_requires=[
