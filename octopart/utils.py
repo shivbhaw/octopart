d756a3119c7d65b37ad771e9e7adaaa870f176f6
Jonas Neubert <jonas@tempoautomation.com>
2017-07-28 14:35:15 -0700

    type annotations and a doctest for utils.py

--- octopart/utils.py
+++ octopart/utils.py
@@ -2,6 +2,7 @@ import collections
 import itertools
 import json
 import logging
+from typing import List
 from urllib.parse import urlencode
 
 
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 URL_MAX_LENGTH = 8000
 
 
-def chunked(list_, chunksize=20):
+def chunked(list_: List, chunksize: int=20) -> List[List]:
     """
     Partitions list into chunks of a given size.
 
@@ -19,19 +20,19 @@ def chunked(list_, chunksize=20):
     than 20 queries in a single request.
 
     Args:
-        list_ (list): list to be partitioned
-        chunksize (int): size of resulting chunks
+        list_: list to be partitioned
+        chunksize: size of resulting chunks
 
     Returns:
         list of lists.
     """
-    chunks = []
+    chunks: List[List] = []
     for i in range(0, len(list_), chunksize):
         chunks.append(list_[i:i + chunksize])
     return chunks
 
 
-def chunk_queries(queries):
+def chunk_queries(queries: List) -> List[List]:
     """
     Partitions list into chunks, and ensures that each chunk is small enough
     to not trigger an HTTP 414 error (Request URI Too Large).
@@ -42,14 +43,14 @@ def chunk_queries(queries):
     Returns:
         list
     """
-    chunks = []
+    chunks: List[List] = []
     # Octopart can only handle 20 queries per request, so split into chunks.
     for chunk in chunked(queries):
         chunks.extend(split_chunk(chunk))
     return chunks
 
 
-def split_chunk(chunk):
+def split_chunk(chunk: List) -> List[List]:
     """
     Split chunk into smaller pieces if encoding the chunk into a URL would
     result in an HTTP 414 error.
@@ -71,12 +72,17 @@ def split_chunk(chunk):
         return [chunk]
 
 
-def flatten(list_of_lists):
+def flatten(list_of_lists: List[List]) -> List:
     return list(itertools.chain(*list_of_lists))
 
 
-def unique(list_):
-    """
-    Removes duplicate entries from list, keeping it in its original order.
+def unique(list_: List) -> List:
+    """Remove duplicate entries from list, keeping it in its original order
+
+    >>> unique([1, 2, 2, 3, 4, 6, 2, 5])
+    [1, 2, 3, 4, 6, 5]
+
+    >>> unique(['bb', 'aa', 'aa', 'aa', 'aa', 'aa', 'bb'])
+    ['bb', 'aa']
     """
     return list(collections.OrderedDict.fromkeys(list_))
