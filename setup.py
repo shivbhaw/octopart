8a0f57ff5bb81f07aef48504016c74ce6c711004
Jonas Neubert <jonas@tempoautomation.com>
2017-08-28 14:21:10 -0700

    version 0.0.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Octopart",
-    version="0.0.19",
+    version="0.0.20",
     author="Sean Adler",
     author_email="sean@tempoautomation.com",
     license="MIT",
